fa989cbbb5da44c604efc25f7d12a5cc0850b8ca
21397 <21397@wc.school.nz>
2025-03-20 11:10:34 +1300

    20/03/25

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import sqlite3
 from sqlite3 import Error
 DATABASE = "tables_tutoring"
 
 app = Flask(__name__)
-
+def is_logged_in():
+    if(session.get('user_id') is None):
+        print('not logged in')
+        return False
+    else:
+        print('logged in')
+        return True
 def connect_database(db_file):
     """
     creates a connection with the database
@@ -21,7 +27,7 @@ def connect_database(db_file):
 
 @app.route('/')
 def render_homepage():
-    return render_template('home.html')
+    return render_template('home.html', logged_in=is_logged_in())
 
 
 @app.route('/menu')
@@ -30,7 +36,23 @@ def render_menu_page():
 
 
 @app.route('/login', methods=['POST', 'GET'])
-def render_login_page():
+def render_login():
+    if request.method == 'POST':
+        email = request.form.get('user_email')
+        password = request.form.get('user_password')
+        con = connect_database(DATABASE)
+        cur = con.cursor()
+        query = 'SELECT fristname, last_name, email, password FROM eamil='
+        cur.execute(query,(email,))
+        results = cur.fetchone()
+        print(results)
+        if password != results[3]:
+            return redirect('/login/message=incorrect+username+or+password')
+        session['email'] = results[2]
+        session['first_name'] = results[0]
+        session['last_name'] = results[1]
+        return redirect('/')
+
     return render_template('login.html')
 
 
@@ -55,6 +77,7 @@ def render_signup_page():
             return redirect("\signup?error=password+is+too+long+,+30+characters+max")
         con = connect_database(DATABASE)
         query_insert = "INSERT INTO user (first_name,last_name, email, password ) VALUES(?,?,?,?)"
+        print('flagged thing kaboom')
         cur = con.cursor()
         cur.execute(query_insert,(fname ,lname ,email ,password))
         con.commit()
